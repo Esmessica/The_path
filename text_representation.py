17150c18563cf605f267d32f80867b2d85ff2259
Anna Lorencová <a.lorencov@seznam.cz>
2023-04-15 19:12:35 +0200

    slow print fix

--- text_representation.py
+++ text_representation.py
@@ -11,15 +11,13 @@ class ConsolePrinter:
     def __str__(self):
         return f"Printing : {self._text}"
 
-    def slow_print(self):
+    def slow_print(self):       # Prints out slowly text into console, text passed from __init__
         print("\t \t", end="")
         for character in self._text:
             print(character, end="", flush=True)
             time.sleep(0.03)
 
 
-c = ConsolePrinter(input("input"))
-c.slow_print()
 
 
 

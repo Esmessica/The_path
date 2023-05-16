e3fddc44a33826b2b1d4ef03069627008187ca57
Anna Lorencová <a.lorencov@seznam.cz>
2023-05-16 23:09:23 +0200

    game flow made into object

--- GameFlow.py
+++ GameFlow.py
@@ -3,29 +3,41 @@ from Forest import *
 from reseter import *
 
 
-"""This file manages the game flow"""
+class Game():
 
-game = GameComponentLogic()
-forest = Forest()
+    """This file manages the game flow"""
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return "Mistic forest game"
+
+    def ingame(self):
+        _game = GameComponentLogic()
+        forest = Forest()
+
+        # print(game.part_start_game())
+        _game.play_forest_sentence(forest.start_car(), 0)
+        input()
+        Clear().cls()
+        _game.play_forest_sentence(forest.forest6(), 1)
+        input()
+        Clear().cls()
+        _game.play_forest_sentence(forest.split_path2(), 2)
+        if _game.direction() == 1:
+            _game.play_forest_sentence(forest.forest5(), 3)
+            input()
+            Clear().cls()
+
+        else:
+            _game.play_forest_sentence(forest.forest7(), 4)
+            input()
+            Clear().cls()
+
+        print("konec testu")
+        input()
 
 
-# print(game.part_start_game())
-game.play_forest_sentence(forest.start_car(), 0)
-input()
-Clear().cls()
-game.play_forest_sentence(forest.forest6(), 1)
-input()
-Clear().cls()
-game.play_forest_sentence(forest.split_path2(), 2)
-if game.direction() == 1:
-    game.play_forest_sentence(forest.forest5(), 3)
-    input()
-    Clear().cls()
-
-else:
-    game.play_forest_sentence(forest.forest7(), 4)
-    input()
-    Clear().cls()
-
-print("konec testu")
-input()
+g = Game()
+g.ingame()
